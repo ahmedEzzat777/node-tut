18b68dce39a5cdf61e1c71e2fed3cc10ef1b0bca
Ahmed Ezzat <a.ezzat.2597@gmail.com>
2021-11-06 14:45:00 +0200

    object validation and put request

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 
 const app = express();
@@ -20,6 +21,7 @@ app.get('/api/courses', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
     let course = courses.find(c => c.id == req.params.id);
+
     if(!course) //404
         res.status(404).send('The course is not found');
     else
@@ -27,13 +29,61 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+
+    const result = validateCourse(req.body);
+
+    if(result.error) {
+        //400 bad request
+        const errors = result.error.details[0].message;
+        res.status(400).send(errors);
+        return;
+    }
+
+
+    // if(!req.body.name || req.body.name.length < 3) {
+    //     //400 bad request
+    //     res.status(400).send('Name is required and should be minimum of 3 characters');
+    //     return;
+    // }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
     };
+
     courses.push(course);
     res.send(course);
 });
+
+
+app.put('/api/courses/:id', (req,res) => {
+    let course = courses.find(c => c.id == req.params.id);
+
+    if(!course) //404
+        res.status(404).send('The course is not found');
+
+    //const result = validateCourse(req.body);
+    const {error} = validateCourse(req.body); //object destructuring
+
+    if(error) {
+        //400 bad request
+        const errors = error.details[0].message;
+        res.status(400).send(errors);
+        return;
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+function validateCourse(course) {
+    const schema = Joi.object({
+        //id: Joi.number().required().min(courses.length),
+        name: Joi.string().min(3).required()
+    });
+
+    return schema.validate(course);
+}
 
 //Port
 const port = process.env.PORT || 3000;
