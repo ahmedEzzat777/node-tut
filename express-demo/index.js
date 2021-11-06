614f19545a9fed986a0f22caa5df71f0f2c9ce87
Ahmed Ezzat <a.ezzat.2597@gmail.com>
2021-11-06 14:56:02 +0200

    end of sec 4

--- express-demo/index.js
+++ express-demo/index.js
@@ -32,12 +32,9 @@ app.post('/api/courses', (req, res) => {
 
     const result = validateCourse(req.body);
 
-    if(result.error) {
+    if(result.error)
         //400 bad request
-        const errors = result.error.details[0].message;
-        res.status(400).send(errors);
-        return;
-    }
+        return res.status(400).send(result.error.details[0].message);
 
 
     // if(!req.body.name || req.body.name.length < 3) {
@@ -57,22 +54,29 @@ app.post('/api/courses', (req, res) => {
 
 
 app.put('/api/courses/:id', (req,res) => {
-    let course = courses.find(c => c.id == req.params.id);
+    let course = courses.find(c => c.id == req.params.id); // or c.id === parseInt(req.params.id)
 
     if(!course) //404
-        res.status(404).send('The course is not found');
+        return res.status(404).send('The course is not found');
 
     //const result = validateCourse(req.body);
     const {error} = validateCourse(req.body); //object destructuring
 
-    if(error) {
-        //400 bad request
-        const errors = error.details[0].message;
-        res.status(400).send(errors);
-        return;
-    }
+    if(error) 
+        return res.status(400).send(error.details[0].message);
 
     course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('/api/courses/:id', (req,res) => {
+    let course = courses.find(c => c.id == req.params.id);
+
+    if(!course) //404
+        return res.status(404).send('The course is not found');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
     res.send(course);
 });
 
