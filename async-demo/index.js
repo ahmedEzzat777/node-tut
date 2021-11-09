d5bb9e8b96eceefd26e0f3ac98794515cf36041f
Ahmed Ezzat <a,ezzat,2597@gmail.com>
2021-11-09 22:27:58 +0200

    named functions

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,9 +1,20 @@
 console.log('before');
-getUser(1, user => {
-    console.log('User', user);
-    getRepositories(user.gitHubUsername, repos => console.log('repos', repos));
-});
+getUser(1, displayUser);
 console.log('After');
+
+function displayUser(user){
+    console.log('User', user);
+    getRepositories(user.gitHubUsername, displayRepos);
+}
+
+function displayRepos(repos) {
+    console.log('repos', repos);
+    getCommits(repos[0], displayCommits);
+}
+
+function displayCommits(commits) {
+    console.log('commits', commits);
+}
 
 function getUser(id, callback) {
 
@@ -18,4 +29,11 @@ function getRepositories(username, callback) {
         console.log('getting a list of repositories from gh api...');
         callback(['repo1', 'repo2', 'repo3']);
     }, 2000);
+}
+
+function getCommits(repo, callback) {
+    setTimeout(() => {
+        console.log('getting a list of commits from gh api...');
+        callback(['commit1', 'commit2']);
+    }, 1000);
 }
