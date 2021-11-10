2ec612d4fa2c54bf541e5e1048a9bba8dac819d0
Ahmed Ezzat <ahmed.ezzat.2597@gmail.com>
2021-11-10 23:05:11 +0200

    query filtering

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -40,7 +40,13 @@ async function getCourses() {
     const courses = await Course
         //.find({author:'Mosh', isPublished: true})
         //.find({price: {$gte: 10, $lte: 20}}) //greater than or eq 10 less than or eq 20
-        .find({price: {$in: [10,15,20]}}) //10 or 15 or 20
+        //.find({price: {$in: [10,15,20]}}) //10 or 15 or 20
+        // .find()
+        // .or([{author:'Mosh'},{isPublished:true}])
+        // .and([])
+        //.find({author: /^Mosh/}) //starts with Mosh
+        //.find({author:/Hamedani$/i}) //ends with hamedani case insen
+        .find({author:/.*Mosh.*/i})
         .limit(10)
         .sort({name:1})
         .select({name:1, tags:1});
