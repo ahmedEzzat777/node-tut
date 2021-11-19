28ae4586105c200a8be07cc4abf2d41f055bfe0b
Ahmed Ezzat <a.ezzat.2597@gmail.com>
2021-11-19 21:27:12 +0200

    logging uncaught exceptions and unhandled promises

--- vidly/index.js
+++ vidly/index.js
@@ -10,9 +10,25 @@ const rentals = require('./routes/rentals');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
 const error = require('./middleware/error');
-
 const app = express();
 const mongoose = require('mongoose');
+
+new winston.ExceptionHandler(winston.createLogger({exitOnError:true}))
+    .handle(new winston.transports.File({filename:'uncaught.log'}));
+
+// process.on('uncaughtException', (err) => {
+//     winston.error(err.message, err);
+//     winston.error(err.message, err, () => {
+//         process.exit(1);
+//     });
+// });
+
+// process.on('unhandledRejection', (err) => {
+//     console.log('unhandled Rejection');
+//     winston.error(err.message, err, () => {
+//         process.exit(1);
+//     });
+// });
 
 winston.add(new winston.transports.File({
     filename:'logfile.log',
@@ -26,6 +42,10 @@ winston.add(new winston.transports.MongoDB({
     db:'mongodb://localhost:27017/vidly',
     level:'info'
 }));
+
+// throw new Error('uncaught'); // handled by process.on('uncaughtException',..)
+// const p = Promise.reject(new Error('uncaught promise rejection'));
+// p.then(()=> console.log('done'));
 
 if(!config.get('jwtPrivateKey')) {
     console.error('FATAL ERROR: jwtPrivateKey not set');
